8ace9689682648a567938db46fce31769d49e1b8
Mohan Gupta <mohangupta@live.com>
2025-04-14 16:53:14 +1000

    now working

--- src/customui.zig
+++ src/customui.zig
@@ -9,16 +9,19 @@ const std = @import("std");
 //     try testing.expect(add(3, 7) == 10);
 // }
 
-
-fn getFolder(allocator: std.mem.Allocator, folder_name: []const []const u8) ![]const u8 {
+fn getFolder(allocator: std.mem.Allocator, folder_components: []const []const u8) ![]const u8 {
     const user_profile_folder = try std.process.getEnvVarOwned(allocator, "USERPROFILE");
     defer allocator.free(user_profile_folder);
-    const absolute_folder_path = try std.fs.path.join(allocator, .{user_profile_folder, folder_name});
+    var components = std.ArrayList([]const u8).init(allocator);
+    defer components.deinit();
+    try components.append(user_profile_folder);
+    try components.appendSlice(folder_components);
+    const absolute_folder_path = try std.fs.path.join(allocator, folder_components);
     return absolute_folder_path;
 }
 
 pub fn checkConfigExists(allocator: std.mem.Allocator) !bool {
-    const downloads_folder = try getFolder(allocator, &.{"Downloads"} );
+    const downloads_folder = try getFolder(allocator, &.{ "Downloads", "boo" });
     defer allocator.free(downloads_folder);
     //std.debug.print("{s}", .{downloads_folder});
     std.log.err("This is an err message - {s}", .{downloads_folder});
